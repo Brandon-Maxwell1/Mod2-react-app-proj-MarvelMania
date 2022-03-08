d9489a2b90d11c2ae0ae0bfad810ca32294afff5
Brandon Maxwell <theill1b22@gmail.com>
2022-03-08 12:50:52 -0600

    pulled from github so that ReadMe will not get overwritten, started creating auto email push for new user accounts, installed emailjs

--- src/pages/NewUserLogin/index.js
+++ src/pages/NewUserLogin/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import { send } from 'emailjs-com';
+import{ init } from '@emailjs/browser';
+init("0b2V77y6Al_RlIYmQ");
 
 const NewUserLogin = () => {
 
